5711f4cfb6214863da6c4c16cd3be287bc57d97e
Roman <example@gmail.com>
2020-03-09 01:53:04 +0300

    zadanie1

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
-[1, 2, 3] => [1, 4, 9]
-[5, 'Hello', 6] => [25, 'HelloHello', 36]
+let money = prompt("Ваш бюджет на месяц?")
+let time = prompt("Введите дату в формате YYYY-MM-DD")
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+}
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце")
+let a2 = prompt("“Во сколько обойдется?”")
+
+expenses: {
+    a1: a2
+    a1: a2
+}
+
+alert(appData.budget / 30)
